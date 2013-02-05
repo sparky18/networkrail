363ed0add819e630e3c975877fac254dab876814
Sparky <spark18-gh@psi.epsilon.org.uk>
2013-02-05 21:49:05 +0000

    serialize network data

--- watch.py
+++ watch.py
@@ -2,12 +2,11 @@
 
 import json
 import logging
+import pickle
 import pprint
 import re
 import sys
 import traceback
-
-sys.path.append('/home/dgeo2/cave/stompest')
 
 from stompest.config import StompConfig
 from stompest.sync import Stomp
@@ -17,12 +16,10 @@ matplotlib.use('Agg')
 import networkx as nx
 import matplotlib.pyplot as plt
 
-seen = []
-
 class networkMapper(object):
 	def __init__(self):
-		self._seen = []
-		self._G = nx.DiGraph()
+		self._edges = []
+		# self._G = nx.DiGraph()
 		self._client = None
 		self._graphFlag = False
 
@@ -35,16 +32,25 @@ class networkMapper(object):
 
 		self._client = client
 
+	def loadEdges(self):
+		try:
+			self._edges = pickle.load(open('edges', 'r'))
+			pprint.pprint(self._edges)
+			self._graphFlag = True
+		except:
+			print >> sys.stderr, 'Could not load edges'
+			self._edges = []
+
 	def updateGraph(self, area, _from, _to):
 		try:
 			if area != 'LB':
 				return
 
-			mFrom = re.match('(\d*)', _from)
+			mFrom = re.match('(\d+)', _from)
 			if mFrom is None:
 				print _from
 				return
-			mTo = re.match('(\d*)', _to)
+			mTo = re.match('(\d+)', _to)
 			if mTo is None:
 				print _to
 				return
@@ -54,23 +60,29 @@ class networkMapper(object):
 
 			pair = (iFrom, iTo)
 			if iFrom < 150:
-				if pair not in seen:
-					seen.append(pair)
-					self._G.add_edge(pair[0], pair[1])
+				if pair not in self._edges:
+					self._edges.append(pair)
 					self._graphFlag = True
+
 		except Exception, e:
 			print e
+			print area, _from, _to
 			print 'erk'
 
 	def outputGraph(self):
 		if self._graphFlag:
-			G = self._G
+			G = nx.DiGraph()
 			plt.clf()
+			for edge in self._edges:
+				G.add_edge(edge[0], edge[1])
+
 			pos = nx.graphviz_layout(G, prog='dot')
 			nx.draw_networkx_nodes(G,pos)
 			nx.draw_networkx_edges(G,pos)
 			nx.draw_networkx_labels(G,pos)
 
+			fig = matplotlib.pyplot.gcf()
+			fig.set_size_inches(10.0, 10.0)
 			plt.axis('off')
 			plt.savefig("simple_path.png") # save as png
 			print 'Graph Updated'
@@ -116,6 +128,9 @@ class networkMapper(object):
 	def disconnect(self):
 		self._client.disconnect()
 
+	def dumpEdges(self):
+		pickle.dump(self._edges, open('edges', 'w'))
+
 def main():
 
 
@@ -127,11 +142,12 @@ def main():
 
 	m = networkMapper()
 	m.configureClient('networkrail-data@psi.epsilon.org.uk', passcode)
+	m.loadEdges()
 	m.go()
 
 
 	m.disconnect()
-
+	m.dumpEdges()
 
 if __name__ == '__main__':
 	main()
